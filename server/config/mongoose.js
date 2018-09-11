d56c3d7dcfd024adaf856ccbc513e56bbba7f1c9
Jesse Ma <j.masonoma17@gmail.com>
2018-09-11 11:19:39 -0700

    Update mongoose.js with proper security protocol-connecting to mlab

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,11 +1,16 @@
+let mlab = require('./mlabConfig.js');
 // Require mongoose
 var mongoose = require('mongoose');
 // require the fs module for loading model files
 var fs = require('fs');
 // require path for getting the models path
 var path = require('path');
-// Connect to mongoose
-mongoose.connect('mongodb://localhost:27017/moviesearch');
+// Connect to mongoose or mlab
+mongoose.connect(`mongodb://${mlab.dbUser}:${mlab.dbPassword}@ds151602.mlab.com:51602/moviesearch`, { autoIndex: false }).then((success) => {
+  console.log('Successful connection!');
+}).catch((error) => {
+  console.log(error);
+})
 // create a variable that points to the path where all of the models live
 var models_path = path.join(__dirname, './../models');
 // Use native promises
--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 // User Schema
 const UserSchema = mongoose.Schema({
